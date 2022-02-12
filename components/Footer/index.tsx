2aa43de9aa510a0c93481bbc82d33119e5f15a9a
raeyoung-kim <760kry@gmail.com>
2022-02-12 19:20:25 +0900

    footer 추가

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Footer: React.FC = () => {
-  return <footer></footer>;
+  return (
+    <footer className="bg-[#181616] h-[200px]">
+      <div className="max-w-[1200px] m-auto pt-16">
+        <p className="font-nanum text-[20px] whitespace-nowrap text-white text-center">
+          모바일 초대장
+        </p>
+        <p className="mt-5 font-sanspro text-center text-[#999999]">
+          Copyrignt ©️ RaeYoung.Kim
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
